6bad9446f54f54286af99f0128d77502300e31c8
abhishekmatcha <seshaabhishek@gmail.com>
2017-03-01 17:07:07 +0530

    Add files via upload

---
+++ String_Test.java
@@ -0,0 +1,18 @@
+class String_Test{
+  public static void main(String args[])
+  {
+    boolean same;
+	String st1="cherry";
+	String st2="cherry";
+	if(st1.equals(st2))
+	{
+		same=true;
+		System.out.println(same);
+	}
+	else
+	{
+		same=false;
+		System.out.println(same);
+	}
+  }
+}
